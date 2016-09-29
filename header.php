97a3a42327cb3afa00a3c07f96a14e91bd2bf525
Netherwarlord <nether.2.ather@gmail.com>
2016-09-28 20:39:40 -0400

    And we have a title!

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Undecided</a>
+          <a class="navbar-brand" href="#">NetherLabs</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
